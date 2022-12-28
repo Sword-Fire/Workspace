4c3b006b1e34f2602885dc48074573241a69a6f7
Leon <geekmc2022@gmail.com>
2022-12-28 22:31:15 +0800

    feat: 在workspace统一管理了gradle依赖版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,36 +7,76 @@ plugins {
     kotlin("jvm") version "1.7.22"
 }
 
-group = "net.geekmc"
-
 repositories {
     mavenCentral()
 }
 
+group = "net.geekmc"
+
+val subExtensions = listOf(
+    "turing-core",
+    "athena"
+)
+
+// 配置所有项目共用的工件版本。
+// ext格式： version.工件名
+allprojects {
+
+    val dependencyToVersionMap = mutableMapOf<String, String>()
+
+    fun addArtifact(name: String, version: String) {
+        ext["version.$name"] = version
+        dependencyToVersionMap[name] = version
+    }
+
+    addArtifact("kotlinx-coroutines-core", "1.6.4")
+    addArtifact("kotlinx-serialization-core", "1.4.1")
+    addArtifact("kotlinx-serialization-json", "1.4.1")
+    addArtifact("kotlin-scripting-common", "1.7.22")
+    addArtifact("kotlin-scripting-jvm", "1.7.22")
+    addArtifact("kotlin-scripting-dependencies", "1.7.22")
+    addArtifact("kotlin-scripting-dependencies-maven", "1.7.22")
+    addArtifact("kotlin-scripting-jvm-host", "1.7.22")
+    addArtifact("snakeyaml", "1.33")
+    addArtifact("adventure-text-minimessage", "4.12.0")
+    addArtifact("KStom", "f02e4c21d4")
+    addArtifact("kaml", "0.49.0")
+    addArtifact("log4j-core", "2.19.0")
+    addArtifact("log4j-slf4j-impl", "2.19.0")
+    addArtifact("clikt", "3.5.0")
+
+    ext["version.dependencyToVersionMap"] = dependencyToVersionMap
+
+}
+
 tasks.create("buildAndCopyExtensions") {
-    fun copyShadowJarInDir(src: java.nio.file.Path, dest: java.nio.file.Path, overwrite: Boolean = true) {
+
+    fun copyJarInDir(src: java.nio.file.Path, dest: java.nio.file.Path, overwrite: Boolean = true) {
         runCatching {
-            Files.walk(src).forEach { a ->
-                if (a.fileName.toString().endsWith("-all.jar")) {
-                    val b = Paths.get(dest.toString() + a.toString().substring(src.toString().length))
-                    if (a != src) {
-                        if (overwrite) {
-                            Files.copy(a, b, StandardCopyOption.REPLACE_EXISTING)
-                        } else {
-                            Files.copy(a, b)
-                        }
+            // 完全遍历源目录。
+            Files.walk(src).forEach { file ->
+                if (file.fileName.toString().endsWith(".jar")) {
+                    // 阻止自拷贝。
+                    if (file == src) {
+                        error("Src and dest are the same: $src")
                     }
-                    println("Copy from $a to $b")
+                    // 当前遍历文件在源目录中的路径所对应的在目标目录中的路径。
+                    val destPath = Paths.get(dest.toString() + file.toString().substring(src.toString().length))
+                    if (overwrite) {
+                        Files.copy(file, destPath, StandardCopyOption.REPLACE_EXISTING)
+                    } else {
+                        Files.copy(file, destPath)
+                    }
+                    println("Copy ${file.fileName} to $destPath")
                 }
             }
         }.onFailure { it.printStackTrace() }
     }
 
-    val extensionProjects = listOf(
-        childProjects["turing-core"]!!, childProjects["athena"]!!
-    )
+    val extensionProjects = subExtensions.map { childProjects[it]!! }
 
-    dependsOn(":turing-core:shadowJar", ":athena:shadowJar")
+    // 运行前先编译所有拓展。
+    dependsOn(":turing-core:build", ":athena:build")
 
     doLast {
         val runDir = project.rootDir.toPath().toAbsolutePath().normalize().resolve("run")
@@ -44,11 +84,12 @@ tasks.create("buildAndCopyExtensions") {
         Files.createDirectories(extensionsDir)
         extensionProjects.map {
             val libsDir = it.buildDir.toPath().resolve("libs")
-            copyShadowJarInDir(libsDir, extensionsDir)
+            copyJarInDir(libsDir, extensionsDir)
         }
     }
 }
 
+// runServer任务会将所有拓展的jar文件拷贝到run目录下，然后运行服务端。
 tasks.create("runServer") {
     dependsOn(tasks.findByName("buildAndCopyExtensions"))
 
